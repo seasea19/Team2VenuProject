65f4170e5f2469b6d10883ac6d39996d9e045d37
Shirley Ni <snssatb@gmail.com>
2021-01-20 22:54:02 -0500

    revised footer

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -1,14 +1,39 @@
-import { grey } from '@material-ui/core/colors';
+/* import { grey } from '@material-ui/core/colors'; */
+import '../../index.css';
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import { Tooltip } from '@material-ui/core';
 
 const Footer = () => {
     return (
-        <footer style = {{backgroundColor:'grey'}}>
+        <footer className = "bck_grey">
             <Fade delay={500}>
-                <div className="font_righteous footer_logo_venue">BootCamp Countdown</div>
-                <div className="footer_copyright">JJAY TTP, Team 2. All rights reserved &copy</div>
-                <div className="footer_copyright">Cenk Cafer, Olviia-Nikol Andriievska, Shirley Ni, Jason Orr, Jayzy Padilla, and Simranjeet Singh</div>
+                <div className="font_righteous footer_logo_venue">
+                    <Tooltip title="Check out our Github!">
+                
+                    
+                    <a href = "https://github.com/SimronJ/Team2VenuProject"
+                    style ={{
+                        color: "#FFF"
+                        
+                        }}
+                    >BootCamp Countdown
+                    </a>
+                </Tooltip>
+                </div>
+                <br></br>
+                <div className="footer_copyright">JJAY TTP, Team 2. All rights reserved ©</div>
+                <div className="footer_copyright">
+                    <ul style ={{listStyleType: "none"}}>
+                        <li>Cenk Cafer</li>
+                        <li>Olviia-Nikol Andriievska</li>
+                        <li>Shirley Ni</li>
+                        <li>Jason Orr</li>
+                        <li>Jayzy Padilla</li>
+                        <li>Simranjeet Singh</li>
+
+                    </ul>
+                </div>
             </Fade>
         </footer>
     );

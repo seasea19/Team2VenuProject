aa2ddb53a848cf4373992c5107d217a39c3d9b0f
Jason Orr <jason.orr94@gmail.com>
2021-01-21 00:15:54 -0500

    modified highlights section

--- src/components/Highlights/Description.js
+++ src/components/Highlights/Description.js
@@ -5,7 +5,6 @@ const Description = () => {
     return (
         <Fade>
             <div className="center_wrapper">
-                <h2>Highlights</h2>
                 <div className="highlight_description">
                     Some filler text - whatever
                 </div>
